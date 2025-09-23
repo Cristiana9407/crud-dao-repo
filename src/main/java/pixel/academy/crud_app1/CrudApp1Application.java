0e0f56ce30a006916c76095b9da60227363e78e6
Cristiana Arseri <cristianazubcu192@gmail.com>
2025-09-23 17:53:09 +0300

    feat: add new feature for creating student

--- src/main/java/pixel/academy/crud_app1/CrudApp1Application.java
+++ src/main/java/pixel/academy/crud_app1/CrudApp1Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import pixel.academy.crud_app1.dao.StudentDAO;
+import pixel.academy.crud_app1.entity.Student;
 
 @SpringBootApplication
 public class CrudApp1Application {
@@ -25,8 +26,15 @@ public class CrudApp1Application {
 
         //cream un nobiect Student
 
+        System.out.println("Creating new student object...");
+        Student newStudent = new Student("John", "Doe", "john@pixelacademy.md");
+
         //Salvam obiectul Student in baza de date folosind DAO
 
+        System.out.println("Saving the student...");
+        studentDAO.save(newStudent);
+
         //display ID of saved student
+        System.out.println("Saved student.Generated id: " + newStudent.getId());
     }
 }
